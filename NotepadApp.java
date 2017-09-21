35d1ab64bdf9822db68b3384fcfbac3328367923
Jagielloofficial <jagiello@list.ru>
2017-09-21 20:07:24 +0300

    test

---
+++ NotepadApp.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class NotepadApp {
+	
+	
+	
+	public static void main(String[] args) {
+		NotepadConsoleUI ui = new NotepadConsoleUI();
+		NotepadConsoleUI ui2 = new NotepadConsoleUI();
+		NotepadConsoleUI ui3 = new NotepadConsoleUI();
+		ui2.start();
+		
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Reminder.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Date;
+
+public class Reminder extends Record {
+	private String reminderDate;
+	
+	public Reminder(String message) {
+		super(message);
+	}
+
+	public String getReminderDate() {
+		return reminderDate;
+	}
+
+	public void setReminderDate(String reminderDate) {
+		this.reminderDate = reminderDate;
+	}
+}
